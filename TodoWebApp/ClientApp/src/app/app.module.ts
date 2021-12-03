7df0265216a9d4fcbf537da9cae8a9d1d2ad9cb2
ishizaki <Keisuke.Ishizaki@jp.ricoh.com>
2021-12-03 13:29:38 +0900

    interactionType:InteractionType.Redirect

--- TodoWebApp/ClientApp/src/app/app.module.ts
+++ TodoWebApp/ClientApp/src/app/app.module.ts
@@ -30,7 +30,7 @@ export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
   protectedResourceMap.set(protectedResources.todoListApi.endpoint, protectedResources.todoListApi.scopes);
 
   return {
-    interactionType: InteractionType.Popup,
+    interactionType: InteractionType.Redirect,
     protectedResourceMap
   };
 }
